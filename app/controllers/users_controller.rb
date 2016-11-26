0bdf255c92a961355166c936ece9e799e492f06c
bshap93 <bshap93@gmail.com>
2016-11-26 13:47:46 -0600

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,20 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:create, :update, :destroy, :show]
-  def new
-    @user = User.new
-  end
-
-  def create
-  end
-
-  def update
-  end
-
-  def edit
-  end
-
-  def destroy
-  end
 
   def index
+    @users = User.all
   end
 
   def show
-  end
-
-  private
-  def user_params
-    params.require(:user).permit(:email, :name, :password, :role)
-  end
-
-  def set_user
     @user = User.find(params[:id])
+    authorize @user
   end
+
+  # private
+  # def user_params
+  #   params.require(:user).permit(:email, :name, :password, :role)
+  # end
+  #
+  # def set_user
+  #   @user = User.find(params[:id])
+  # end
 end
